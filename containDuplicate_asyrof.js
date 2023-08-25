5d61de1368b3b8ba396497451cb6e45e00aa5874
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-26 00:40:08 +0700

    logictest

---
+++ containDuplicate_asyrof.js
@@ -0,0 +1,33 @@
+/**
+ * write a function that returns true if there's duplicate in the array, and false otherwise.
+ * SEE EXAMPLE BELLOW!
+ * 
+ * 
+Example
+console.log(containsDuplicate([1, 2, 3, 1])); // Output: true
+console.log(containsDuplicate([1, 2, 3, 4])); // Output: false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // Output: true
+
+ * Determines if the array contains any duplicate value.
+
+ * @param {number[]} nums - The input array of integers.
+ * @return {boolean} Returns true if the array contains any duplicate value, false otherwise.
+ */
+function containsDuplicate(nums) {
+  // Your logic here
+
+  const numSet = new Set();
+
+  for (const num of nums) {
+    if (numSet.has(num)) {
+      return true; // Ada elemen yang duplikat
+    }
+    numSet.add(num); // Tambahkan elemen ke set
+  }
+
+  return false; // Tidak ada elemen yang duplikat
+}
+
+console.log(containsDuplicate([1, 2, 3, 1])); // Output: true
+console.log(containsDuplicate([1, 2, 3, 4])); // Output: false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // Output: true
