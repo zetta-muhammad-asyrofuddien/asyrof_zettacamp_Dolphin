bd5ef03dba230f72f9b47250e610f7a233fe3fc3
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-30 09:39:16 +0700

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,28 @@
+const songList = [
+  {
+    title: 'Menunggu Kamu',
+    artist: 'Anji',
+    year: 2018,
+    duration: '4:17',
+    genre: 'Pop',
+  },
+  {
+    title: 'Arjuna',
+    artist: 'Dewa 19',
+    year: 2002,
+    duration: '5:13',
+    genre: 'Pop',
+  },
+  {
+    title: 'Gossip Jalanan',
+    artist: 'Slank',
+    year: 2004,
+    duration: '5:13',
+    genre: 'Rock',
+  },
+];
+
+function songBasedArtist(data) {
+  let groupedArtist = [];
+}
+// Print the song list
